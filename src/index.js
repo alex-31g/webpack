de01d5031703ef06dde43bdbbdacf50d10e57a32
alex-31g <al.tsimbalyuk@gmail.com>
2020-08-25 19:05:10 +0300

    updated webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-import one from "./file_1";
+import one from './file_1';
 
 console.log(one + 1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,36 @@
 // модуль path - для работы с путями
 const path = require('path');
 
+// html-webpack-plugin подключаем в поле plugins
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 module.exports = {
+  // context - указывает где лежат все исходники приложения
+  context: path.resolve(__dirname, 'src'),
+
   // mode: 'development', // в каком режиме делать сборку проекта
 
   // входной файл приложения
-  entry: './src/index.js',
+  entry: {
+    main: './index.js',
+  },
 
+  // https://webpack.js.org/configuration/output/
   // output - сообщает webpack, где и как хранить собранные файлы
   output: {
-    // filename - в какой файл собираем проект
-    filename: 'bundle.js',
+    // filename - в какой файл собираем проект.
+    // Здесь происходит считывание всех полей из entry и их сборка.
+    // Паттерн [name] учавствует в создании имени файла -
+    // он динамически указывает на поле внутри entry, то-есть:
+    // поле main + .bundle.js --> получаем файл main.bundle.js
+
+    // filename: '[name].bundle.js',
+
+    // Паттерн [contenthash] - динамически добавляет хэш файла к названию,
+    // таким образом мы всегда делаем пересборку в случаи изменения данных в файле
+    filename: '[name].[contenthash].js',
 
     // path - путь к директории, где будет храниться собранный проект
     // path.resolve() - принимает составные части путей и возвращает абсолютный путь
@@ -21,17 +41,17 @@ module.exports = {
     publicPath: 'dist',
   },
 
-  // module - настройка узловых модулей - помогают работать с файлами разных типов в совместимых с браузерами форматах – JS, CSS и т. д.
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        // указываем, что используем babel-loader для конвертации JS-файла с ES6 в ES5
-        loader: 'babel-loader',
-        exlude: 'node_modules/',
-      },
-    ],
-  },
+  // plugins - содержит список всех плагинов
+  plugins: [
+    new HTMLWebpackPlugin({
+      // Значение для тега <title> index.html страницы - он работает только в случаи отсутствия поля template
+      // Так как у нас есть поле template, то поле title не будет работать
+      title: 'webpack 7',
+      // Указываем путь к index.html
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
 
   // настройка devServer
   devServer: {
