96645dcb81521658cdf909027649f8d223ea877e
alex-31g <al.tsimbalyuk@gmail.com>
2020-09-01 15:46:27 +0300

    added lazy loading

--- src/models/file_1.js
+++ src/models/file_1.js
@@ -1,1 +1,11 @@
 export default 1;
+
+// Динамические импорты + Lazy loading
+
+const x = true;
+
+if (x) {
+  import('lodash').then((_) => {
+    console.log('Lodash', _.random(0, 10, true));
+  });
+}
